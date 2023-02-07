fff1cdb3d51b91395c4d44ba169c9cb94e1597c1
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-02-07 20:30:42 +1100

    Prepare for publishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Box } from './components'
 
-const App = () => <BrowserRouter><Routes><Route path='/' element={<b>Hi</b>} /></Routes></BrowserRouter>
+const App = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route
+                path='/'
+                element={
+                    <>
+                        <Box variant='h1'>Heading 1</Box>
+                        <Box variant='h2'>Heading 2</Box>
+                        <Box variant='h3'>Heading 3</Box>
+                        <Box variant='h4'>Heading 4</Box>
+                        <Box variant='h5'>Heading 5</Box>
+                        <Box variant='p'>Normal text</Box>
+                        <Box margin={{ b: 'xs' }}></Box>
+                        <Box variant='sub'>Subscript text</Box>
+                    </>
+                }
+            />
+        </Routes>
+    </BrowserRouter>
+)
 
 export default App
