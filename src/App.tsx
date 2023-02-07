83e348ba971cd8cc0815cf5a69084f0c24126675
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-02-08 09:55:20 +1100

    Add little icon showcase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Box } from './components'
+import { Box, Icon } from './components'
+import { IconType } from './internal/icons'
 
 const App = () => (
     <BrowserRouter>
@@ -13,7 +14,21 @@ const App = () => (
                         <Box variant='h3'>Heading 3</Box>
                         <Box variant='h4'>Heading 4</Box>
                         <Box variant='h5'>Heading 5</Box>
-                        <Box variant='p'>Normal text</Box>
+                        <Box variant='p'>
+                            <Icon type='warning' /> Normal text <Icon type='edit' />
+                        </Box>
+                        <Box variant='sub'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis aliquam ullam, perferendis id commodi alias minima rerum repellendus perspiciatis veritatis ea quo
+                            maiores unde, eius labore quia doloribus quod praesentium facere numquam voluptates temporibus. Officiis, provident corporis! Rerum pariatur ad, eos sit mollitia id ipsam
+                            nostrum ex minima modi rem reiciendis consectetur! Nihil id, blanditiis vitae ratione ipsam, culpa qui tempore molestias hic ipsum amet iusto voluptates voluptatum modi
+                            nostrum facere repudiandae eligendi, debitis placeat. Iste nesciunt nobis, incidunt praesentium expedita aliquam omnis. Voluptatibus doloribus error quisquam fugiat
+                            corporis repudiandae architecto autem vero vitae dolores, aperiam nulla accusamus eveniet iste, placeat non illum? Excepturi similique culpa natus corrupti praesentium sit.
+                            Quia velit quod tempore nesciunt nam enim ab in. Accusamus quas repellendus numquam nostrum, quod deserunt iste. Aut, animi eveniet fugiat saepe eius tempore dolorem a
+                            laboriosam quo, nam, inventore vero magnam recusandae! Quidem laudantium nisi veritatis, blanditiis cumque officia animi quae commodi minus, molestias dolor voluptates
+                            eaque adipisci eos earum placeat accusantium maiores provident nulla enim. Rerum sequi debitis impedit. Perferendis accusantium ea vel corrupti saepe itaque. Placeat
+                            voluptatem quisquam culpa ratione alias perspiciatis excepturi, nesciunt voluptas ad nulla repellendus minus quas quaerat soluta. Commodi facilis nulla alias ullam!
+                        </Box>
+                        <IconShowcase />
                         <Box margin={{ b: 'xs' }}></Box>
                         <Box variant='sub'>Subscript text</Box>
                     </>
@@ -24,3 +39,91 @@ const App = () => (
 )
 
 export default App
+
+const IconShowcase = () => {
+    const types: IconType[] = [
+        'adjustments',
+        'arrow-down',
+        'arrow-left',
+        'arrow-right',
+        'arrow-up',
+        'burger',
+        'bell',
+        'bell-slash',
+        'bookmark',
+        'bookmark-slash',
+        'calender',
+        'cart',
+        'chat-bubble',
+        'check',
+        'check-circle',
+        'chevron-down',
+        'chevron-left',
+        'chevron-right',
+        'chevron-up',
+        'chevron-up-down',
+        'clipboard',
+        'close',
+        'close-circle',
+        'code-block',
+        'code-bracket',
+        'command-line',
+        'copy',
+        'document',
+        'download',
+        'edit',
+        'ellipsis',
+        'ellipsis-circle',
+        'ellipsis-vertical',
+        'exclamation',
+        'eye',
+        'eye-slash',
+        'expand',
+        'external',
+        'fingerprint',
+        'folder',
+        'filter',
+        'flag',
+        'heart',
+        'home',
+        'info',
+        'link',
+        'list',
+        'lock',
+        'magnify',
+        'map',
+        'map-pin',
+        'microphone',
+        'plane',
+        'minus',
+        'minus-circle',
+        'plus',
+        'plus-circle',
+        'printer',
+        'puzzle',
+        'qr',
+        'question',
+        'queue',
+        'settings',
+        'share',
+        'sound',
+        'silent',
+        'stack',
+        'star',
+        'table',
+        'trash',
+        'unlock',
+        'user',
+        'user-circle',
+        'users',
+        'upload',
+        'warning',
+    ]
+    return (
+        <Box>
+            {types.map(type => (
+                <Icon type={type} />
+            ))}
+        </Box>
+    )
+}
