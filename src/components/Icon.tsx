233a0c77d8554989af819d06b70649f956101ff4
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-02-08 09:46:39 +1100

    Add core icon properties

---
+++ src/components/Icon.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { classNames } from '../internal/class-compiler'
+import { IconSvgPath, IconType } from '../internal/icons'
+
+type IconSize = 'sm' | 'md' | 'lg'
+type IconColor = 'black' | 'white' | 'gray' | 'primary' | 'success' | 'error' | 'warning' | 'inherit'
+
+export interface IconProps {
+    // Properties
+    type: IconType
+    size?: IconSize
+    color?: IconColor
+}
+
+export const Icon: React.FC<IconProps> = ({ type, size = 'md', color = 'black' }) => {
+    return (
+        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className={classNames('inline', IconSizeStyles[size], IconColorStyles[color])}>
+            {IconSvgPath[type]}
+        </svg>
+    )
+}
+
+const IconSizeStyles: Record<IconSize, string> = {
+    sm: 'h-5 w-5',
+    md: 'h-6 w-6',
+    lg: 'h-7 w-7',
+}
+
+const IconColorStyles: Record<IconColor, string> = {
+    black: 'text-black',
+    white: 'text-white',
+    gray: 'text-gray-400',
+    primary: 'text-indigo-600',
+    success: 'text-lime-600',
+    error: 'text-red-600',
+    warning: 'text-amber-600',
+    inherit: 'text-inherit',
+}
