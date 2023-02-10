23dbeec6b62ed0bebc24672e2180df8d51cc65fe
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-02-10 22:46:24 +1100

    Add style to textarea

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { classNames } from '../internal/class-compiler'
 
 type InputDetail = {
     event: any
@@ -13,12 +14,13 @@ export interface TextareaProps {
     readOnly?: boolean
     required?: boolean
     rows?: number
+    fullWidth?: boolean
 
     // Events
     onChange?: (detail: InputDetail) => any
 }
 
-export const Textarea: React.FC<TextareaProps> = ({ value, placeholder, readOnly = false, required = false, rows = 3, onChange }) => {
+export const Textarea: React.FC<TextareaProps> = ({ value, placeholder, readOnly = false, required = false, rows = 3, fullWidth = false, onChange }) => {
     const id = crypto.randomUUID()
 
     const _onChange: React.ChangeEventHandler<HTMLTextAreaElement> = event => {
@@ -31,5 +33,18 @@ export const Textarea: React.FC<TextareaProps> = ({ value, placeholder, readOnly
         }
     }
 
-    return <textarea value={value} placeholder={placeholder} readOnly={readOnly} required={required} rows={rows} onChange={_onChange} />
+    return (
+        <textarea
+            value={value}
+            placeholder={placeholder}
+            readOnly={readOnly}
+            required={required}
+            rows={rows}
+            onChange={_onChange}
+            className={classNames(
+                fullWidth ? 'w-full' : 'w-60',
+                'max-w-full resize appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500',
+            )}
+        />
+    )
 }
