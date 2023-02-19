ba15a2b18c681ab3062fbe3957676ec009627f63
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-02-19 12:44:16 +1100

    Add sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { classNames } from '../internal/class-compiler'
+import { Box } from './Box'
+
+export interface SidebarProps {
+    // Properties
+    title?: string
+
+    // Slots
+    children?: React.ReactNode
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({ title, children }) => {
+    const widthStyles = 'hidden lg:block w-72'
+    // 'w-48 md:w-52 lg:w-60 xl:w-72 2xl:w-80'
+    return (
+        <div className={classNames('flex max-h-full flex-col overflow-hidden overflow-y-auto border-r border-gray-200 bg-white', widthStyles)}>
+            {title && (
+                <Box variant='h5' padding={{ x: 'lg', y: 'lg' }}>
+                    {title}
+                </Box>
+            )}
+            {children}
+        </div>
+    )
+}
